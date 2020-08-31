0730925548586366413ccb505d7c1e199be33a7f
ga74kud <michael.hartmann@berkeley.edu>
2020-08-31 09:40:52 +0200

    some vizdoom tests Signed-off-by: ga74kud <michael.hartmann@berkeley.edu>

--- source/doom_test.py
+++ source/doom_test.py
@@ -3,21 +3,25 @@
 from vizdoom import *
 import random
 import time
-
+from matplotlib import pyplot as plt
 game = DoomGame()
 game.load_config("../input/scenarios/basic.cfg")
 game.init()
 
 
-left =     [1, 0, 0, 0, 0, 0, 0]
-right =    [0, 1, 0, 0, 0, 0, 0]
-shoot =    [0, 0, 1, 0, 0, 0, 0]
-forward=   [0, 0, 0, 1, 0, 0, 0]
-backward=  [0, 0, 0, 0, 1, 0, 0]
-turn_left= [0, 0, 0, 0, 0, 1, 0]
-turn_right=[0, 0, 0, 0, 0, 0, 1]
-actions = [left, right, shoot, forward, backward, turn_left, turn_right]
-
+left =     [1, 0, 0, 0, 0, 0, 0, 0, 0]
+right =    [0, 1, 0, 0, 0, 0, 0, 0, 0]
+shoot =    [0, 0, 1, 0, 0, 0, 0, 0, 0]
+forward=   [0, 0, 0, 1, 0, 0, 0, 0, 0]
+backward=  [0, 0, 0, 0, 1, 0, 0, 0, 0]
+turn_left= [0, 0, 0, 0, 0, 1, 0, 0, 0]
+turn_right=[0, 0, 0, 0, 0, 0, 1, 0, 0]
+look_down=   [0, 0, 0, 0, 0, 0, 0, 1, 0]
+look_up= [0, 0, 0, 0, 0, 0, 0, 0, 1]
+actions = [left, right, shoot, forward, backward, turn_left, turn_right, look_down, look_up]
+#game.set_depth_buffer_enabled(True)
+#game.set_automap_render_textures(True)
+#game.set_screen_format()
 episodes = 10
 for i in range(episodes):
     game.new_episode()
@@ -28,5 +32,11 @@ for i in range(episodes):
         reward = game.make_action(random.choice(actions))
         print("\treward:", reward)
         time.sleep(0.02)
+        depth = state.depth_buffer
+        plt.imshow(depth, interpolation='nearest')
+        plt.show()
+        plt.close()
+        b=3
+
     print("Result:", game.get_total_reward())
     time.sleep(2)
