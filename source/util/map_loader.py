826beda39187f300b9e38c75f21c900e91d14eaf
ga74kud <michaelhartmann10@googlemail.com>
2020-11-05 14:06:58 +0100

    Signed-off-by: ga74kud <michaelhartmann10@googlemail.com>

--- source/util/map_loader.py
+++ source/util/map_loader.py
@@ -13,97 +13,102 @@ import json
 import os
 import sys
 
-def preprocessing():
-    input_file="/home/michael/ros/vifware_data_puntigam/pcd/puntigam_v1.pcd"
-    cloud = PyntCloud.from_file(input_file)
-    converted_triangle_mesh = cloud.to_instance("pyvista", mesh=True)
-    abc=np.array(converted_triangle_mesh.points)
-    dataset = pd.DataFrame({'x': abc[:, 0], 'y': abc[:, 1], 'z': abc[:, 2]})
-    bd=sp.spatial.distance.cdist(abc, [[35, 50, 0]])<20
 
-    compressed_data=[abc[i] for i in range(0, len(abc)) if bd[i]==True]
-    x = da.from_array(compressed_data, chunks=(18000, 3))
-    km = daml.KMeans(n_clusters=100)
-    km.fit(x)
-    if(0):
-        fig, ax = plt.subplots()
-        ax.scatter(x[:, 0], x[:, 1], marker='.', c=km.labels_[:],
+class map_loader(object):
+    def __init__(self, **kwargs):
+        None
+    def preprocessing(self):
+        input_file="/home/michael/ros/vifware_data_puntigam/pcd/puntigam_v1.pcd"
+        cloud = PyntCloud.from_file(input_file)
+        converted_triangle_mesh = cloud.to_instance("pyvista", mesh=True)
+        abc=np.array(converted_triangle_mesh.points)
+        dataset = pd.DataFrame({'x': abc[:, 0], 'y': abc[:, 1], 'z': abc[:, 2]})
+        bd=sp.spatial.distance.cdist(abc, [[35, 50, 0]])<20
+
+        compressed_data=[abc[i] for i in range(0, len(abc)) if bd[i]==True]
+        x = da.from_array(compressed_data, chunks=(18000, 3))
+        km = daml.KMeans(n_clusters=100)
+        km.fit(x)
+        if(0):
+            fig, ax = plt.subplots()
+            ax.scatter(x[:, 0], x[:, 1], marker='.', c=km.labels_[:],
                cmap='viridis', alpha=0.1)
-    #plt.show()
-    newSelObject=pv.PolyData(compressed_data)
-    pv.save_meshio("new.ply", newSelObject)
-    output_file = open('file.bin', 'wb')
-    float_array = array('d', np.random.rand(3))
-    float_array.tofile(output_file)
-    output_file.close()
-    data_segmentation = {"point": np.array(compressed_data), "label_to_names": None}
+        #plt.show()
+        newSelObject=pv.PolyData(compressed_data)
+        pv.save_meshio("new.ply", newSelObject)
+        output_file = open('file.bin', 'wb')
+        float_array = array('d', np.random.rand(3))
+        float_array.tofile(output_file)
+        output_file.close()
+        data_segmentation = {"point": np.array(compressed_data), "label_to_names": None}
 
-    dict = {"data": compressed_data, "label": np.ones((np.size(compressed_data, 0), 1)), "scores": None, "dask_data": x, "kmeans": km}
-    write_pickle(dict, 'important')
-def write_pickle(data, file_name):
-    file = open(file_name, 'wb')
-    cPickle.dump(data, file)
-    file.close()
+        dict = {"data": compressed_data, "label": np.ones((np.size(compressed_data, 0), 1)), "scores": None, "dask_data": x, "kmeans": km}
+        write_pickle(dict, 'important')
+    def write_pickle(self, data, file_name):
+        file = open(file_name, 'wb')
+        cPickle.dump(data, file)
+        file.close()
 
 
-def load_semantic_dataset():
-    ref, km=classify_to_meta()
-    return km
+    def load_semantic_dataset(self):
+        ref, km=self.classify_to_meta()
+        return km
 
-def save_semantic_kmeans(km):
-    ROOT_DIR = "/home/michael/PycharmProjects/voting_reinforcement_learning/"
-    ENVIRONMENT_DIR = ROOT_DIR + "input/environment/"
-    FILE_DIR=ENVIRONMENT_DIR+"/data.json"
-    abc=list(km[0].cluster_centers_)
-    d=dict()
-    for a,b in enumerate(abc):
-        d['%03d' % a]=b.tolist()
-    data_input={"points": d}
-    with open(FILE_DIR, 'w') as f:
-        json.dump(data_input, f)
+    def save_semantic_kmeans(self, km):
+        ROOT_DIR = "/home/michael/PycharmProjects/voting_reinforcement_learning/"
+        ENVIRONMENT_DIR = ROOT_DIR + "input/environment/"
+        FILE_DIR=ENVIRONMENT_DIR+"/data.json"
+        abc=list(km[0].cluster_centers_)
+        d=dict()
+        for a,b in enumerate(abc):
+            d['%03d' % a]=b.tolist()
+        data_input={"points": d}
+        with open(FILE_DIR, 'w') as f:
+            json.dump(data_input, f)
 
-def classify_to_meta():
-    saved_data=open_preprocessed_data()
-    data = saved_data["data"]
-    semantic_label = saved_data["label"]
-    km = saved_data["kmeans"]
-    dask_data=saved_data["dask_data"]
-    npdata=np.array([(data[i][0], data[i][1], data[i][2]) for i in range(0, len(data))])
-    ref=np.array([(npdata[i,:], semantic_label[i]) for i in range(0, len(npdata)) if npdata[i,2]>-1000.0 and npdata[i,2]<1400])
-    daski=(km, dask_data)
-    return ref, daski
-def open_preprocessed_data():
-    file = open('/home/michael/PycharmProjects/voting_reinforcement_learning/source/util/important', 'rb')
-    saved_data = cPickle.load(file)
-    file.close()
-    return saved_data
-def find_data_by_idx(a, b, idx):
-    da.where(a==b[idx])
-def show_pyvista(ref, dask_input):
-    km=dask_input[0]
-    data=dask_input[1].compute()
-    p = pv.Plotter()
-    semantic_label = km.labels_
-    single_classes=np.unique(semantic_label.compute())
-    p.add_mesh(km.cluster_centers_, opacity=1, point_size=12, render_points_as_spheres=True, color="red")
-    for wlt in range(0, 100):
-        sel_idx=da.where(semantic_label==wlt)[0].compute()
-        new_dat=np.array([(data[i][0], data[i][1], data[i][2]) for i in sel_idx])
-        reducedMesh = pv.PolyData(new_dat)
-        p.add_mesh(reducedMesh, opacity=0.5, point_size=3,render_points_as_spheres=True, color=np.random.randn(3))
-    p.show_grid()
-    p.show(screenshot='city2.png')
-
-def show_single():
-    input_file = "haltestelle.ply"
-    cloud = PyntCloud.from_file(input_file)
-    converted_triangle_mesh = cloud.to_instance("pyvista", mesh=True)
-    p = pv.Plotter()
-    p.add_mesh(converted_triangle_mesh, show_edges=True, color=np.random.rand(3), opacity=0.5)
-    p.show_grid()
-    p.show(screenshot='red.png')
+    def classify_to_meta(self):
+        saved_data=self.open_preprocessed_data()
+        data = saved_data["data"]
+        semantic_label = saved_data["label"]
+        km = saved_data["kmeans"]
+        dask_data=saved_data["dask_data"]
+        npdata=np.array([(data[i][0], data[i][1], data[i][2]) for i in range(0, len(data))])
+        ref=np.array([(npdata[i,:], semantic_label[i]) for i in range(0, len(npdata)) if npdata[i,2]>-1000.0 and npdata[i,2]<1400])
+        daski=(km, dask_data)
+        return ref, daski
+    def open_preprocessed_data(self):
+        file = open('/home/michael/PycharmProjects/voting_reinforcement_learning/source/util/important', 'rb')
+        saved_data = cPickle.load(file)
+        file.close()
+        return saved_data
+    def find_data_by_idx(self, a, b, idx):
+        da.where(a==b[idx])
+    def show_pyvista(self, ref, dask_input):
+        km=dask_input[0]
+        data=dask_input[1].compute()
+        self.p = pv.Plotter()
+        semantic_label = km.labels_
+        single_classes=np.unique(semantic_label.compute())
+        self.p.add_mesh(km.cluster_centers_, opacity=1, point_size=12, render_points_as_spheres=True, color="red")
+        for wlt in range(0, 100):
+            sel_idx=da.where(semantic_label==wlt)[0].compute()
+            new_dat=np.array([(data[i][0], data[i][1], data[i][2]) for i in sel_idx])
+            reducedMesh = pv.PolyData(new_dat)
+            self.p.add_mesh(reducedMesh, opacity=0.05, point_size=3,render_points_as_spheres=True, color=np.random.randn(3))
+        self.p.show_grid()
+    def show_plot(self):
+        self.p.show(screenshot='city2.png')
+    def show_single(self):
+        input_file = "haltestelle.ply"
+        cloud = PyntCloud.from_file(input_file)
+        converted_triangle_mesh = cloud.to_instance("pyvista", mesh=True)
+        p = pv.Plotter()
+        p.add_mesh(converted_triangle_mesh, show_edges=True, color=np.random.rand(3), opacity=0.5)
+        p.show_grid()
+        p.show(screenshot='red.png')
 
 if __name__ == '__main__':
+    obj=map_loader()
     if(0):
-        preprocessing()
-    show_semantic_dataset()
+        obj.preprocessing()
+    obj.show_semantic_dataset()
