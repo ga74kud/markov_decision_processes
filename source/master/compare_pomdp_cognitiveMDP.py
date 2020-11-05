91337be263329dd74756cdb6d63aa58c70b6fc85
ga74kud <michaelhartmann10@googlemail.com>
2020-11-05 14:34:35 +0100

    visual and use mdp
    Signed-off-by: ga74kud <michaelhartmann10@googlemail.com>

--- source/master/compare_pomdp_cognitiveMDP.py
+++ source/master/compare_pomdp_cognitiveMDP.py
@@ -6,16 +6,19 @@ class service_handler(object):
     def __init__(self, **kwargs):
         None
     def use_all_solvers(self):
-        problem={'type': 'pomdp', 'rewards': {'24': 10}}
-        obj_pomdp=service_POMDP()
-        obj_pomdp.set_problem_type(problem)
-        obj_pomdp.new_problem()
-        ideal_path=obj_pomdp.start_mdp()
+        self.use_mdp()
         b=2
         #problem_type = {'type': 'cognitive_mdp', 'rewards_body': {'24': 10}, 'rewards_cortex': {'52': 10}}
         #service_CognitiveMDP(problem_type)
         #problem_type = {'type': 'scm'}
         #service_scmMDP(problem_type)
+    def use_mdp(self):
+        problem={'type': 'pomdp', 'rewards': {'24': 10}}
+        obj_pomdp=service_POMDP()
+        obj_pomdp.set_problem_type(problem)
+        obj_pomdp.new_problem()
+        ideal_path=obj_pomdp.start_mdp()
+        return ideal_path
 
 if __name__ == '__main__':
     obj_map=map_loader()
